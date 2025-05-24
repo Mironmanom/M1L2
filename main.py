cdb93b1d847b4383d584d13b1c5a69245f39a0bf
Mironmanom <mironlearn@gmail.com>
2025-05-24 17:00:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import random
+import telebot
+    
 
-Beleborda = "_-<>,.~`#@%$abcdefghigklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-Long = int(input('Длинна пароля:'))
-Password = ''
-for i in range (Long):
-    Password += random.choice(Beleborda)
-print(Password)
+bot = telebot.TeleBot('Token')
+
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+# Запуск бота
+bot.polling()
